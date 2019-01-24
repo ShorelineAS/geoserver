3739dd23af4eaa9a3619d27f5e715ef0f087da94
Ronni H. Baslund <ronnibaslund@gmail.com>
2019-01-24 10:39:22 +0100

    init

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Check the GeoServer version specified in the Dockerfile and substitute the number in the globals starting with OLD.
+
+# This represents the version we need GeoServer to move up to. ie the latest stable version
+BUGFIX=2
+MINOR=14
+MAJOR=2
+
+# Build Geoserver
+echo "Building GeoServer using the specified version "
+sed -i "s/${OLD_MAJOR}.${OLD_MINOR}.${OLD_BUGFIX}/${MAJOR}.${MINOR}.${BUGFIX}/g" Dockerfile
+docker build --build-arg GS_VERSION=${MAJOR}.${MINOR}.${BUGFIX} -t kartoza/geoserver:${MAJOR}.${MINOR}.${BUGFIX} .
+
+# Build Arguments - To change the defaults when building the image
+#need to specify a different value.
+```
+--build-arg ORACLE_JDK=true
+--build-arg COMMUNITY_MODULES=true
+--build-arg TOMCAT_EXTRAS=false
+--build-arg WAR_URL=http://downloads.sourceforge.net/project/geoserver/GeoServer/<GS_VERSION>/geoserver-<GS_VERSION>-war.zip
+```
